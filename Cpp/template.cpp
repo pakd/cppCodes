fbe67be78a3db5c9818283c75b85a5dedcfa2a0f
pakd <deepak.kumar@FireEye.com>
2017-02-17 21:08:09 +0530

    template example added

---
+++ Cpp/template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// template definition cannot appear in a block.
+template <typename T>
+T AddTwoNums(T first, T second)
+{
+    return first+second;
+}
+int main()
+{
+
+
+    cout << AddTwoNums(2.5,3.4) << endl;
+
+    cout << AddTwoNums(12,38);
+
+}
