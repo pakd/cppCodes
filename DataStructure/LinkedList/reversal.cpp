f960b67feccb8a9abe7d0d48186e68db00f125a6
pakd <deepak.kumar@FireEye.com>
2017-03-13 10:48:01 +0530

    linked list reversal added

---
+++ DataStructure/LinkedList/reversal.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "linked_list_library.hpp"
+using namespace std;
+
+
+Node* reverseLinkedList(Node* head)
+{
+    Node* curr = head;
+    Node* prev = NULL;
+    Node* Next;
+
+    while(curr)
+    {
+        Next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = Next;
+    }
+
+    head = prev;
+}
+int main()
+{
+    BasicLinkedList myList;
+    Node* head = NULL;
+    head = myList.insert(head,1);
+    head = myList.insert(head,2);
+    head = myList.insert(head,3);
+    head = myList.insert(head,4);
+    head = reverseLinkedList(head);
+    myList.display(head);
+
+}
