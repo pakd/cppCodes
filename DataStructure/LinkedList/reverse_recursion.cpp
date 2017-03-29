68ad60efb0d8791d7270e12a5fd5a81b05ab264d
pakd <deepak.kumar@FireEye.com>
2017-03-29 21:38:19 +0530

    reverse recursion

---
+++ DataStructure/LinkedList/reverse_recursion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "linked_list_library.hpp"
+using namespace std;
+Node* head = NULL;
+void reverseRecursionLinkedList(Node* p)
+{
+    if(p->next == NULL)
+    {
+        head = p;
+        return;
+    }
+    reverseRecursionLinkedList(p->next);
+    Node* q = p->next;
+    q->next = p;
+    p->next = NULL;
+
+}
+int main()
+{
+    BasicLinkedList myList;
+    //Node* head = NULL;
+    head = myList.insert(head,1);
+    head = myList.insert(head,2);
+    head = myList.insert(head,3);
+    head = myList.insert(head,4);
+    head = myList.insert(head,5);
+    reverseRecursionLinkedList(head);
+    myList.display(head);
+
+}
