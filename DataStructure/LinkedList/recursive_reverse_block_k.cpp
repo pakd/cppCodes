a22de1c67b16a06a64a968f3a4607f606d920ee8
pakd <deepak.kumar@FireEye.com>
2017-03-31 00:45:03 +0530

    reverse in k block

---
+++ DataStructure/LinkedList/recursive_reverse_block_k.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "linked_list_library.hpp"
+using namespace std;
+
+
+Node* reverseLinkedList(Node* head, int k)
+{
+    Node* curr = head;
+    Node* prev = NULL;
+    Node* Next;
+    int count = 0;
+
+    while(curr && count < k)
+    {
+        Next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = Next;
+        count++;
+    }
+    if(Next)
+    {
+        head->next = reverseLinkedList(Next,k);
+    }
+    head = prev;
+
+    return head;
+}
+int main()
+{
+    BasicLinkedList myList;
+    Node* head = NULL;
+    head = myList.insert(head,1);
+    head = myList.insert(head,2);
+    head = myList.insert(head,3);
+    head = myList.insert(head,4);
+    head = myList.insert(head,5);
+    head = myList.insert(head,6);
+    head = myList.insert(head,7);
+    head = myList.insert(head,8);
+    head = myList.insert(head,9);
+    head = myList.insert(head,10);
+    head = reverseLinkedList(head,3);
+    myList.display(head);
+
+}
