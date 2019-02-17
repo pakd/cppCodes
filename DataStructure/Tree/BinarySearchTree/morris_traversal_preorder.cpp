e7876be9517ed2f0dd5842fc0e47a912ad274552
pakd <deepak.kumar.cse12@gmail.com>
2019-02-17 17:04:43 +0530

    morris preorder traversal added to BT

---
+++ DataStructure/Tree/BinarySearchTree/morris_traversal_preorder.cpp
@@ -0,0 +1,67 @@
+#include "bst_library.hpp"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void MorrisTraversalInorder(Node* root)
+{
+    if(!root) return;
+    Node* curr = root;
+
+    while(curr)
+    {
+        if(!curr->left)
+        {
+            // If left subtree doesnt exist print current's data and move to right of current.
+            cout << curr->data << "\t";
+            curr = curr->right;
+        }
+        else
+        {
+            // take pre which will be the inordor predecessor of curr in left subtree
+            Node* pre = curr->left;
+            while(pre->right && pre->right != curr)
+            {
+                // go to the rightmost child of left subtree
+                pre = pre->right;
+            }
+
+            if(!pre->right)  // link formation
+            {
+                cout << curr->data << "\t";
+                pre->right = curr;
+                curr=curr->left;
+            }
+            else // break link
+            {
+                pre->right = NULL;
+                curr = curr->right;    // left subtree is already printed
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    BasicTree myTree;
+    Node* root = NULL;
+
+
+    vector<int>  dpk{100,50,150,30,60,120,180,170,210};
+
+    for(int i=0; i< dpk.size(); i++)
+    {
+
+        root = myTree.insert(root, dpk[i]);
+    }
+
+    //display level order
+    myTree.displayLevelOrder(root);
+
+    cout << endl;
+
+    MorrisTraversalInorder(root);
+
+
+}
