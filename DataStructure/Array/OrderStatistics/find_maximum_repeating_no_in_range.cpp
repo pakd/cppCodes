814e3760a7de7b847739cdb1b57669d400c5505f
pakd <deepak.kumar.cse12@gmail.com>
2019-03-16 19:49:13 +0530

    maximum repeating element in a range 0 to k

---
+++ DataStructure/Array/OrderStatistics/find_maximum_repeating_no_in_range.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// using no extra space
+int maxRepeatingNo(int *arr, int n, int k)
+{
+    for(int i=0;i<n;i++)
+    {
+        arr[arr[i]%k] += k;
+    }
+
+    // find index maximum repeating element
+    int max = arr[0], result = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            result = i;
+        }
+    }
+
+    /* Uncomment this code to get the original array back
+       for (int i = 0; i< n; i++)
+          arr[i] = arr[i]%k; */
+
+    // Return index of the maximum element
+    return result;
+}
+
+/* Driver code */int main()
+{
+    int arr[] = {2, 3, 3, 5, 3, 4, 1, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 8;
+    cout << "The maximum repeating number is " << maxRepeatingNo(arr, n, k) << endl;
+    return 0;
+}
