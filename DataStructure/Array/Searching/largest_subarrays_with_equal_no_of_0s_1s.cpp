75a2e7fcdf026f9b7b70113765a38d51e392a9ed
pakd <deepak.kumar.cse12@gmail.com>
2019-03-17 23:06:50 +0530

    largest subarrays with equal no of 0s and 1s added

---
+++ DataStructure/Array/Searching/largest_subarrays_with_equal_no_of_0s_1s.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+void findSubArray(int *arr, int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        arr[i] = (arr[i]==0)? -1:1;
+    }
+
+    int sum[n];
+    sum[0] = arr[0];
+    for(int i=1; i<n; i++)
+    {
+        sum[i] = arr[i] + sum[i-1];
+    }
+
+    // element , index
+    map<int, int> m;
+
+    int maxVal = -1;
+    int startIndex = -1;
+    int endIndex = -1;
+
+    // this is helpful in getting subarrays starting from index 0
+    m[0] = -1;
+    for(int i=0;i<n;i++)
+    {
+        if( m.find(sum[i]) != m.end() )
+        {
+            int index = m[sum[i]];
+            int diff = i - index;
+            if(diff > maxVal)
+            {
+                maxVal = diff;
+                startIndex = index;
+                endIndex = i;
+            }
+        }
+        else
+        {
+            m[sum[i]] = i;
+        }
+
+    }
+
+    cout << startIndex + 1 << "\t" << endIndex  << endl;
+
+}
+int main()
+{
+    int arr[] =  {1, 0, 0, 1, 0, 1, 1}; //{1, 0, 1, 1, 1, 0, 0};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    findSubArray(arr, size);
+    return 0;
+}
