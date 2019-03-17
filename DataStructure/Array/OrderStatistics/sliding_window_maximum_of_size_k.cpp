ac91e1fcb543d7c3b43c8065bc98935b837dd6d8
pakd <deepak.kumar.cse12@gmail.com>
2019-03-17 16:40:14 +0530

    sliding window maximum of size k added

---
+++ DataStructure/Array/OrderStatistics/sliding_window_maximum_of_size_k.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+
+int printKMax(int *arr, int n, int k)
+{
+    deque<int> dq;
+    int i=0;
+    for(i=0;i<k;i++)
+    {
+        // remove all smaller elements
+        while( !dq.empty() && arr[i] >= arr[dq.back()])
+        {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+    }
+
+
+    for( ; i<n ; i++)
+    {
+        cout  << arr[dq.front()] << "\t";
+
+        // remove all elements from older windows
+        while(!dq.empty() && dq.front() <= i-k)
+        {
+            dq.pop_front();
+        }
+
+        // remove all smaller elements
+        while( !dq.empty() && arr[i] >= arr[dq.back()])
+        {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+    }
+    cout << arr[dq.front()] << "\t";
+}
+
+int main()
+{
+    int arr[] = {12, 1, 78, 90, 57, 89, 56};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 3;
+    printKMax(arr, n, k);
+    return 0;
+}
