583a60bab1e78261ac6247f8a9c3a1336c922f86
pakd <deepak.kumar.cse12@gmail.com>
2019-03-24 15:39:01 +0530

    merge overlapping intervals added

---
+++ DataStructure/Array/RangeQuery/merge_intervals.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+struct Interval
+{
+    int start;
+    int end;
+};
+
+bool compare(Interval A, Interval B)
+{
+    return (A.start < B.start);
+}
+void mergeIntervals(Interval *arr, int n)
+{
+    stack<Interval> st;
+
+    sort(arr, arr + n, compare);
+
+    st.push(arr[0]);
+
+    for(int i=1; i<n;i++)
+    {
+        Interval topInterval = st.top();
+        if(arr[i].start <= topInterval.end)
+        {
+            topInterval.end = max(arr[i].end, topInterval.end);
+            st.pop();
+            st.push(topInterval);
+        }
+        else
+        {
+            st.push(arr[i]);
+        }
+    }
+
+    while (!st.empty())
+    {
+        Interval t = st.top();
+        cout << "[" << t.start << "," << t.end << "] ";
+        st.pop();
+    }
+}
+
+int main()
+{
+    //Interval arr[] =  { {6,8}, {1,9}, {2,4}, {4,7} }; //test 1
+    Interval arr[] =  { {2,4}, {5,7}, {8,10}, {9,15} }; // test 2
+    int n = sizeof(arr)/sizeof(arr[0]);
+    mergeIntervals(arr, n);
+    return 0;
+}
