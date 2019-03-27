7a0b695a56a8c0d5e5f50f5ff308d13e2d3c208e
pakd <deepak.kumar.cse12@gmail.com>
2019-03-27 23:49:37 +0530

    reach desired array added

---
+++ DataStructure/Array/Optimization/reach_desired_array.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+int oddCount(int *arr, int n)
+{
+    int count = 0;
+
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]&1)
+        {
+            count++;
+            arr[i]--;
+        }
+    }
+
+    return count;
+}
+
+void halfArray(int *arr, int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        arr[i] = arr[i]/2;
+    }
+
+}
+
+bool ifAllZero(int *arr, int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]!=0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+int countMinOperations(int *arr, int n)
+{
+    int minCount = 0;
+    while(1)
+    {
+        if(ifAllZero(arr, n))
+        {
+            return minCount;
+        }
+        int odd = oddCount(arr, n);
+        if(odd > 0)
+        {
+
+            minCount+=odd;
+        }
+        else
+        {
+            minCount++;
+            halfArray(arr, n);
+        }
+    }
+}
+
+int main()
+{
+     int arr[] = {2, 1};//{2, 3};//{16, 16, 16};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "Minimum number of steps required to "
+           "get the given target array is "
+         << countMinOperations(arr, n);
+    return 0;
+}
