9667775e0765aacc7f7ba28a7755ef8d2b588855
pakd <deepak.kumar.cse12@gmail.com>
2019-03-28 23:41:17 +0530

    smallest array with sum greater than x added

---
+++ DataStructure/Array/Optimization/minimum_length_subarray_with_sum_greater_than_x.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int smallestSubWithSum(int *arr, int n, int x)
+{
+    int curr_sum = 0;
+    int min_length = INT_MAX;
+
+    int start = 0, end = 0;
+
+    while(end < n)
+    {
+        if(curr_sum < 0)
+        {
+            start  = end;
+            curr_sum = 0;
+        }
+
+        else if(curr_sum <= x)
+        {
+            curr_sum+= arr[end];
+            end++;
+        }
+        else
+        {
+            min_length = min(min_length, end - start);
+            curr_sum -= arr[start];
+            start++;
+        }
+
+    }
+    return min_length;
+}
+
+int main()
+{
+    int arr1[] = {- 8, 1, 4, 2, -6};
+    int x = 6;
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    int res1 = smallestSubWithSum(arr1, n1, x);
+    (res1 == n1+1)? cout << "Not possible\n" :
+                    cout << res1 << endl;
+
+                     int arr2[] = {1, 10, 5, 2, 7};
+    int n2 = sizeof(arr2)/sizeof(arr2[0]);
+    x  = 9;
+    int res2 = smallestSubWithSum(arr2, n2, x);
+    (res2 == n2+1)? cout << "Not possible\n" :
+                    cout << res2 << endl;
+
+    int arr3[] = {1, 11, 100, 1, 0, 200, 3, 2, 1, 250};
+    int n3 = sizeof(arr3)/sizeof(arr3[0]);
+    x  = 280;
+    int res3 = smallestSubWithSum(arr3, n3, x);
+    (res3 == n3+1)? cout << "Not possible\n" :
+                    cout << res3 << endl;
+
+    return 0;
+}
