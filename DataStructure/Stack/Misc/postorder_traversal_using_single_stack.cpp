c31af5dafc7ec75acc278f9e230129af792388c3
pakd <deepak.kumar.cse12@gmail.com>
2019-04-04 00:24:14 +0530

    postorder traversal using single stack added

---
+++ DataStructure/Stack/Misc/postorder_traversal_using_single_stack.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// A tree node
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+};
+
+Node* newNode(int data)
+{
+    Node* node = new Node();
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+
+void postOrderIterative(Node* root)
+{
+    Node* current = root;
+    stack<Node*> st;
+
+    // while current is not null or stack is not empty
+    while(current || !st.empty())
+    {
+        // traversing left subtree
+        if(current)
+        {
+            st.push(current);
+            current = current->left;
+        }
+
+        // traversing right subtree, current is null
+        else
+        {
+            Node* temp = st.top()->right;
+
+            // no right child, so we can print the node
+            if(temp == NULL)
+            {
+                temp = st.top();
+                st.pop();
+                cout << temp->data << " ";
+
+                // printing parents of right subtree
+                while(!st.empty() && temp == st.top()->right)
+                {
+                    temp = st.top();
+                    st.pop();
+                    cout << temp->data << " ";
+                }
+            }
+
+            // if temp is not null or search for left subtree of right child
+            else
+            {
+                current = temp;
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    Node* root = NULL;
+    root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+
+    postOrderIterative(root);
+
+    return 0;
+}
