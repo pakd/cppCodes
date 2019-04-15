ad74a7f98f063b5141a973e77b5f9c3ffc494e86
pakd <deepak.kumar.cse12@gmail.com>
2019-04-15 23:15:51 +0530

    rat in a maze problem added

---
+++ DataStructure/Graph/rat_in_a_maze_problem.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+#define N 4
+
+/* A utility function to print solution matrix sol[N][N] */
+void printSolution(int sol[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << sol[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool isSafe(int maze[N][N], int x, int y)
+{
+    // if (x,y outside maze) return false
+    if(x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 1)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool solveMazeUtil(int maze[N][N], int x, int y, int sol[N][N])
+{
+    if(x == N-1 && y == N-1)
+    {
+        sol[x][y] = 1;
+        return true;
+    }
+
+    if(isSafe(maze, x, y))
+    {
+        // mark x,y as part of solution path
+        sol[x][y] = 1;
+
+        /* Move forward in x direction */
+        if(solveMazeUtil(maze, x + 1, y, sol))
+        {
+            return true;
+        }
+
+        /* If moving in x direction doesn't give solution then
+           Move down in y direction  */
+        if(solveMazeUtil(maze, x, y + 1, sol))
+        {
+            return true;
+        }
+
+         /* If none of the above movements work then BACKTRACK:
+            unmark x,y as part of solution path */
+        sol[x][y] = 0;
+        false;
+    }
+}
+
+
+void solveMaze(int maze[N][N])
+{
+    int sol[N][N] = {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    };
+
+    if(!solveMazeUtil(maze, 0, 0, sol))
+    {
+        cout << "Solution doesn't exist";
+        return;
+    }
+    else
+    {
+        printSolution(sol);
+    }
+}
+
+int main()
+{
+    // 0 = gray cell, not allowed
+    // 1 = allowed
+    int maze[N][N]  =  {
+        {1, 0, 0, 0},
+        {1, 1, 0, 1},
+        {0, 1, 0, 0},
+        {1, 1, 1, 1}
+    };
+
+    solveMaze(maze);
+    return 0;
+}
