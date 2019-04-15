b1d1573f7e58e9a330aa1fcf218b9346597e5668
pakd <deepak.kumar.cse12@gmail.com>
2019-04-15 22:59:00 +0530

    topological sort added

---
+++ DataStructure/Graph/topological_sorting.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <list>
+#include <stack>
+
+using namespace std;
+
+
+class Graph
+{
+public:
+
+    int v; // no of vertices
+    list<int> *adj;
+    int *visited;
+
+    Graph(int v)
+    {
+        this->v = v;
+        adj = new list<int>[v];
+        visited = new int[v];
+        for(int i=0; i<v; i++)
+        {
+            visited[i] = false;
+        }
+    }
+
+    void addEdge(int v, int w)
+    {
+        adj[v].push_back(w);
+    }
+
+    topologicalSortUtil(int s, stack<int> &st)
+    {
+        visited[s] = true;
+
+        for(auto i : adj[s])
+        {
+            if(!visited[i])
+            {
+                topologicalSortUtil(i, st);
+            }
+        }
+
+        st.push(s);
+    }
+
+
+    void topologicalSort()
+    {
+        stack<int> st;
+
+        // call topologicalSortUtil for every vertex
+        for(int i=0;i<v;i++)
+        {
+            if(!visited[i])
+            {
+                topologicalSortUtil(i, st);
+            }
+        }
+
+        while(!st.empty())
+        {
+            cout << st.top() << " ";
+            st.pop();
+        }
+
+    }
+};
+
+int main()
+{
+    Graph g(6);
+    g.addEdge(5, 2);
+    g.addEdge(5, 0);
+    g.addEdge(4, 0);
+    g.addEdge(4, 1);
+    g.addEdge(2, 3);
+    g.addEdge(3, 1);
+
+
+
+    cout << "Following is a Topological Sort of the given graph \n";
+    g.topologicalSort();
+
+    return 0;
+}
