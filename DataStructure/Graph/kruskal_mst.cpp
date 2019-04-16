1f6d8ee6b8ca45cadc0c96428ade4e24b47fb664
pakd <deepak.kumar.cse12@gmail.com>
2019-04-17 01:09:38 +0530

    kruskal added

--- DataStructure/Graph/kruskal_mst.cpp
+++ DataStructure/Graph/kruskal_mst.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+typedef pair<int, int> iPair;
 
 struct subset
 {
@@ -16,6 +19,7 @@ public:
     // first = parent
     // second = rank
     subset *subsets;
+    vector<pair<int, iPair>> edges;
 
     Graph(int V)
     {
@@ -27,6 +31,11 @@ public:
             subsets[i].rank = 0;
         }
 
+    }
+
+    void addEdge(int src, int dest, int weight)
+    {
+        edges.push_back(make_pair(weight, make_pair(src, dest)));
     }
 
     // if i = subsets[i].parent then it is single element in set or root
@@ -50,13 +59,13 @@ public:
         if(subsets[xRoot].rank < subsets[yRoot].rank)
         {
             subsets[xRoot].parent = yRoot;
-            // subsets[yRoot].rank++;
+             //subsets[yRoot].rank++;
         }
         // make xRoot parent
         else if(subsets[xRoot].rank > subsets[yRoot].rank)
         {
             subsets[yRoot].parent = xRoot;
-            // subsets[xRoot].rank++;
+             //subsets[xRoot].rank++;
         }
         // if both ranks equal, make anyone parent
         // lets make xRoot parent
@@ -67,38 +76,57 @@ public:
         }
     }
 
-    bool isCycle(vector<pair<int, int>> &edges)
+    int kruskalMST()
     {
-        for(auto i : edges)
-        {
-            int x = find(i.first);
-            int y = find(i.second);
-            // cout << i.first << " " << i.second << endl;
+        int mst_wt = 0;
 
-            if(x == y)
+        sort(edges.begin(), edges.end());
+
+        for(auto it:edges)
+        {
+            int xRoot = find(it.second.first);
+            int yRoot = find(it.second.second);
+
+            // if xRoot == yRoot, then cycle, don't include the edge
+            if(xRoot != yRoot)
             {
-                return true;
+                // include the edge
+                cout << it.second.first << "->" << it.second.second << endl;
+                mst_wt += it.first;
+
+                Union(xRoot, yRoot);
             }
-            Union(x, y);
         }
-        return false;
+
+        return mst_wt;
     }
 };
-
 int main()
 {
-    vector<pair<int, int>> edges;
-    edges.push_back(make_pair(0, 1));
-    edges.push_back(make_pair(1, 2));
-    edges.push_back(make_pair(0, 2)); // can comment this to get , "Graph doesn't contain cycle"
 
-    int V = 3;
-    Graph g(3);
+    int V = 9;
+    Graph g(V);
 
-    if (g.isCycle(edges))
-        cout << "Graph contains cycle";
-    else
-        cout << "Graph doesn't contain cycle";
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 7, 8);
+    g.addEdge(1, 2, 8);
+    g.addEdge(1, 7, 11);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 8, 2);
+    g.addEdge(2, 5, 4);
+    g.addEdge(3, 4, 9);
+    g.addEdge(3, 5, 14);
+    g.addEdge(4, 5, 10);
+    g.addEdge(5, 6, 2);
+    g.addEdge(6, 7, 1);
+    g.addEdge(6, 8, 6);
+    g.addEdge(7, 8, 7);
+
+
+    cout << "Edges of MST are \n";
+    int mst_wt = g.kruskalMST();
+
+    cout << "\nWeight of MST is " << mst_wt;
 
     return 0;
 }
