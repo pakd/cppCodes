f251b7cf01e5130fdc7d59b8c74f1e0b628df403
pakd <deepak.kumar.cse12@gmail.com>
2019-05-22 22:52:37 +0530

    using namespace std removed

--- Algorithm/Searching/binary_search.cpp
+++ Algorithm/Searching/binary_search.cpp
@@ -1,20 +1,28 @@
 #include<iostream>
-using namespace std;
 int BinarySearch(int arr[],int left, int right, int key)
 {
-    if(right<left) return -1;
+    if(left > right) return -1;
+
+    int mid = (left + right)/2;
+
+    if(arr[mid] == key)
+    {
+        return mid;
+    }
+
+    else if(arr[mid] > key)
+    {
+        return BinarySearch(arr, left, mid-1, key);
+    }
+
     else
     {
-        int mid= (left+right)/2;
-        if(arr[mid]==key) return mid;
-        else if(arr[mid]>key) BinarySearch(arr,left,mid-1,key);
-        else BinarySearch(arr,mid+1,right,key);
-     }
+        return BinarySearch(arr, mid+1, right, key);
+    }
 }
 int main()
 {
     int arr[]={3,6,9,10,23,45,56,67,89};
-    cout<<BinarySearch(arr,0,8,89);
-
-
+    std::cout << BinarySearch(arr,0,8,10);
+    return 0;
 }
